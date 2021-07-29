8cfe5ec53c01f01cc7c94e6ff9f41a63bb6f269c
Peter Daino Otieno <peterdaino0@gmail.com>
2021-07-29 19:46:56 +0300

    Update login.js

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -24,7 +24,8 @@ import axios from "axios";
       }).then(res => {
           localStorage.setItem('cool-jwt',res.data.accessToken);
           this.props.history.push("/secured")
-      }).catch(error =>{
+      })
+      .catch(error =>{
           console.log(error)
           swal({
             title: "Oops!",
@@ -33,17 +34,24 @@ import axios from "axios";
             button: false,
           });
       })
+      function success () {
+        swal({
+          title: "Login Successful",
+          text:"Please wait, We're redirecting you to the dashboard",
+          icon:"success",
+        });
+      }
   };
 
   render() {
       return (
           <div className="card">
-              <form onSubmit={this.submit}>
+              <form id="login_form" onSubmit={this.submit}>
                   <label htmlFor="email">Email</label>
-                  <input placeholder="email/Username" type="text" name="email" onChange={this.onChange} value={this.state.email}/>
+                  <input id="usernameOrEmail" className="form-control" placeholder="email/Username" type="text" name="email" onChange={this.onChange} value={this.state.email}/>
                   <label htmlFor="password">Password</label>
-                  <input type="password" name="password" placeholder="your password" onChange={this.onChange} value={this.state.password}/>
-                  <input type="submit" value="Login"/>
+                  <input type="password" className="form-control" id="password" name="password" placeholder="your password" onChange={this.onChange} value={this.state.password}/>
+                  <input className="btn btn-primary btn-block" type="submit" value="Sign In"/>
               </form>
           </div>
       );
